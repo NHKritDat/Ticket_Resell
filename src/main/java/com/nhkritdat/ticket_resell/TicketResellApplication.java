49e1742a1b9d2b7bd67677efd64a42a5ed6f780e
Krit Dat <dat01102003@gmail.com>
2024-11-10 22:12:21 +0700

    feat/create-project

---
+++ src/main/java/com/nhkritdat/ticket_resell/TicketResellApplication.java
@@ -0,0 +1,13 @@
+package com.nhkritdat.ticket_resell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketResellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketResellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhkritdat/ticket_resell/TicketResellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhkritdat.ticket_resell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketResellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
